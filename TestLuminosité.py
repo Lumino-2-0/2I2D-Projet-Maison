26cc16aaf556301dac2733059d437f9de9b77559
Lumastor <lumino110908@gmail.com>
2025-11-18 11:09:51 +0100

    Refactor LDR reading and add LED control logic
    Updated LDR setup and added LED control based on voltage threshold.

--- TestLuminosité.py
+++ TestLuminosité.py
@@ -1,8 +1,27 @@
-from machine import ADC
-import time
- 
-ldr = ADC(27)
+from machine import Pin, ADC
+from time import sleep
+
+# DEL sur GP16
+led = Pin(16, Pin.OUT)
+
+# LDR sur GP26 (ADC0)
+ldr = ADC(26)
+
+# Seuil (à ajuster selon mesures)
+# Ici on prend un seuil ~1.5 V, à adapter
+SEUIL_V = 2.4   # tension en volts
+
 while True:
-    print(ldr.read_u16())
-    time.sleep(2)
-*
+    raw = ldr.read_u16()
+    voltage = raw * 3.3 / 65535
+    
+    print("Raw :", raw, "| Tension :", voltage, "V")
+    
+    if voltage > SEUIL_V:
+        # sombre -> allumer la LED
+        led.value(1)
+    else:
+        # clair -> éteindre la LED
+        led.value(0)
+    
+    sleep(0.1)
